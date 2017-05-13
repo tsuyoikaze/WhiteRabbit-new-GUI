3674f548b7c57a112261f19a5ea3b31518b92a29
Siwei Xu <tuyoikaze@gmail.com>
2017-05-13 14:49:38 -0700

    file loader updated

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
-
-
+import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
+
+
+import org.ohdsi.rabbitInAHat.dataModel.Database;
+import org.ohdsi.rabbitInAHat.dataModel.ETL;
+import org.ohdsi.rabbitInAHat.dataModel.Field;
+import org.ohdsi.rabbitInAHat.dataModel.Table;
+import org.ohdsi.whiteRabbit.ObjectExchange;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 import javafx.application.*;
 import javafx.event.EventHandler;
@@ -21,6 +34,54 @@ public class Main extends Application{
 	private Stage primaryStage;
 	private BorderPane mainLayout;
 	
+	FileChooser chooser;
+	
+	private String chooseFile(boolean saveMode, ExtensionFilter filter) {
+		
+		String result = null;
+
+		if (chooser == null) {
+			chooser = new FileChooser();
+		}
+		
+		chooser.setSelectedExtensionFilter(filter);
+		
+		File selectedFile = saveMode ? chooser.showSaveDialog(primaryStage) : chooser.showOpenDialog(primaryStage);
+		
+		return selectedFile.getAbsolutePath();
+	}
+	
+	private void doOpenScanReport(String filename) {
+		if (filename != null) {
+			
+			mainLayout.setCursor(Cursor.WAIT);
+			ETL etl = new ETL();
+			try {
+				etl.setSourceDatabase(Database.generateModelFromScanReport(filename));
+				etl.setTargetDatabase(ObjectExchange.etl.getTargetDatabase());
+				//tableMappingPanel.setMapping(etl.getTableToTableMapping());
+				//TODO render mapping
+				ObjectExchange.etl = etl;
+			}
+			 catch (Exception e) {
+				e.printStackTrace();
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Error");
+				alert.setHeaderText(null);
+				alert.setContentText("Invalid File Format");
+				alert.showAndWait();
+			}
+			
+			 mainLayout.setCursor(Cursor.DEFAULT);
+			 try {
+				Scene newScene = new Scene((Pane) FXMLLoader.load(Main.class.getResource("screens/view/Screen2.fxml")));
+				primaryStage.setScene(newScene);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 	
 	
 	public void start(final Stage primaryStage) throws IOException {
@@ -31,9 +92,11 @@ public class Main extends Application{
 		mainLayout = loader.load();
 		Scene scene = new Scene(mainLayout);
 		newProjectBtn = (Button) scene.lookup("#new_project_btn");
-		openExcelBtn = (Button) scene.lookup("#new_project_btn");
+		openExcelBtn = (Button) scene.lookup("#open_excel_btn");
+		System.err.println(openExcelBtn);
 		openProjectBtn = (Button) scene.lookup("#new_project_btn");
 		
+		//TODO should not have this in future
 		newProjectBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
 			public void handle(MouseEvent arg0) {
@@ -45,6 +108,15 @@ public class Main extends Application{
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+			}
+			
+		});
+		
+		openExcelBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			public void handle(MouseEvent arg0) {
+				System.err.println("in ");
+				doOpenScanReport(chooseFile(false, new FileChooser.ExtensionFilter("Excel spreadsheet", "*.xlsx")));
 			}
 			
 		});
